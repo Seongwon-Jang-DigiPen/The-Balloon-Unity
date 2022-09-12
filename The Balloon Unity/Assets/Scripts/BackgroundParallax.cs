548dda2f0a3222f266a2a415a62929149e7658c9
Seongwon Jang <seongwon.jang@digipen.edu>
2022-09-12 16:12:44 +0900

    city background image

--- The Balloon Unity/Assets/Scripts/BackgroundParallax.cs
+++ The Balloon Unity/Assets/Scripts/BackgroundParallax.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundParallax : MonoBehaviour
+namespace Platformer.View
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class BackgroundParallax : MonoBehaviour
     {
-        
+        public Vector3 movementScale = Vector3.one;
+
+        Transform _camera;
+
+        void Awake()
+        {
+            _camera = Camera.main.transform;
+        }
+
+        void LateUpdate()
+        {
+            transform.position = Vector3.Scale(_camera.position, movementScale);
+        }
+
     }
 }
