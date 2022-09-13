6626a2c39e21cccc5eb75933fa86807aeae5cd8c
Seongwon Jang <seongwon.jang@digipen.edu>
2022-09-13 20:25:55 +0900

    cloud move block done

--- The Balloon Unity/Assets/Scripts/Block/SwitchMoveBlock.cs
+++ The Balloon Unity/Assets/Scripts/Block/SwitchMoveBlock.cs
@@ -9,10 +9,15 @@ public class SwitchMoveBlock : MoveBlock
     SpriteRenderer spriteRenderer;
     public Sprite activateSprite = null;
     public Sprite deactivateSprite = null;
+    Animator animator;
+    public RuntimeAnimatorController activateAni;
+    public RuntimeAnimatorController deactivateAni;
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         spriteRenderer.sprite = deactivateSprite;
+        animator.runtimeAnimatorController = deactivateAni;
     }
     protected override void FixedUpdate()
     {
@@ -21,6 +26,7 @@ public class SwitchMoveBlock : MoveBlock
             if(spriteRenderer.sprite != activateSprite)
             {
                 spriteRenderer.sprite = activateSprite;
+                animator.runtimeAnimatorController = activateAni;
             }
             base.FixedUpdate();
         }
@@ -29,6 +35,7 @@ public class SwitchMoveBlock : MoveBlock
             if (spriteRenderer.sprite != deactivateSprite)
             {
                 spriteRenderer.sprite = deactivateSprite;
+                animator.runtimeAnimatorController = deactivateAni;
             }
             rigid.velocity = Vector2.zero;
         }
