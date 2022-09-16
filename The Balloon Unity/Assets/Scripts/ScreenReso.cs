1a1823abdecbc71898eaee021476725b2deb84d8
Sangbeom Kim <sangbeom.kim@digipen.edu>
2022-09-16 09:04:14 +0900

    finalfinal

--- The Balloon Unity/Assets/Scripts/ScreenReso.cs
+++ The Balloon Unity/Assets/Scripts/ScreenReso.cs
@@ -7,7 +7,7 @@ public class ScreenReso : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1280, 720, true);
+        Screen.SetResolution(1920, 1080, true);
         Screen.SetResolution(Screen.width, (Screen.width * 16) / 9, true);
     }
 
